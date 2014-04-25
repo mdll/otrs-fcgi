4edeeea929874c310296af9d590cd566cd324119
dan <daniel.lindner@mac.com>
2014-04-25 13:12:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Install otrs, running with nginx via fast-cgi
+=============================================
+For running otrs with nginx, you have to do some dependencies:
+
+#1. Required Software-Packages
+
+``
+root@otrs:~$ apt-get install mysql-server nginx spawn-fcgi libnet-dns-perl libio-socket-ssl-perl libnet-ldap-perl libgd-text-perl libgd-graph-perl libpdf-api2-perl libsoap-lite-perl libuser libyaml-libyaml-perl libtext-csv-perl libtext-csv-xs-perl libfcgi-perl libmail-imapclient-perl libjson-xs-perl libcrypt-eksblowfish-perl libencode-hanextra-perl
+``
+
+#2. download otrs in current version
+
+``
+root@otrs:~$ wget http://ftp.otrs.org/pub/otrs/otrs-3.x.x.tar.gz
+root@otrs:~$ cd /var/local/
+root@otrs:~$ tar -xzf otrs-3.x.x.tar.gz
+root@otrs:~$ ln -s otrs-3.x.x otrs
+root@otrs:~$ chmod -R www-data.www-data otrs*
+``
---
+++ bin/fastcgi-wrapper.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl
+
+use FCGI;
+use Socket;
+use POSIX qw(setsid);
+
+require 'syscall.ph';
+
+&daemonize;
+
+END() { } BEGIN() { }
+*CORE::GLOBAL::exit = sub { die "fakeexit\nrc=".shift()."\n"; }; 
+eval q{exit}; 
+if ($@) { 
+	exit unless $@ =~ /^fakeexit/; 
+};
+
+&main;
+
+sub daemonize() {
+    chdir '/'                 or die "Can't chdir to /: $!";
+    defined(my $pid = fork)   or die "Can't fork: $!";
+    exit if $pid;
+    setsid                    or die "Can't start a new session: $!";
+    umask 0;
+}
+
+sub main {
+        $socket = FCGI::OpenSocket( "/var/run/otrs/otrs.cgi.sock", 10 );
+        $request = FCGI::Request( \*STDIN, \*STDOUT, \*STDERR, \%req_params, $socket );
+        if ($request) { request_loop()};
+            FCGI::CloseSocket( $socket );
+}
+
+sub request_loop {
+        while( $request->Accept() >= 0 ) {
+            
+           $stdin_passthrough ='';
+           $req_len = 0 + $req_params{'CONTENT_LENGTH'};
+           if (($req_params{'REQUEST_METHOD'} eq 'POST') && ($req_len != 0) ){ 
+                my $bytes_read = 0;
+                while ($bytes_read < $req_len) {
+                        my $data = '';
+                        my $bytes = read(STDIN, $data, ($req_len - $bytes_read));
+                        last if ($bytes == 0 || !defined($bytes));
+                        $stdin_passthrough .= $data;
+                        $bytes_read += $bytes;
+                }
+            }
+
+            if ( (-x $req_params{SCRIPT_FILENAME}) &&
+                 (-s $req_params{SCRIPT_FILENAME}) &&
+                 (-r $req_params{SCRIPT_FILENAME})
+            ){
+		pipe(CHILD_RD, PARENT_WR);
+		my $pid = open(KID_TO_READ, "-|");
+		unless(defined($pid)) {
+			print("Content-type: text/plain\r\n\r\n");
+                        print "Error: CGI app returned no output - Executing $req_params{SCRIPT_FILENAME} failed !\n";
+			next;
+		}
+		if ($pid > 0) {
+			close(CHILD_RD);
+			print PARENT_WR $stdin_passthrough;
+			close(PARENT_WR);
+
+			while(my $s = <KID_TO_READ>) { print $s; }
+			close KID_TO_READ;
+			waitpid($pid, 0);
+		} else {
+	                foreach $key ( keys %req_params){
+        	           $ENV{$key} = $req_params{$key};
+                	}
+	                if ($req_params{SCRIPT_FILENAME} =~ /^(.*)\/[^\/]+$/) {
+                        	chdir $1;
+                	}
+
+			close(PARENT_WR);
+			close(STDIN);
+			syscall(&SYS_dup2, fileno(CHILD_RD), 0);
+			exec($req_params{SCRIPT_FILENAME});
+			die("exec failed");
+		}
+            } 
+            else {
+                print("Content-type: text/plain\r\n\r\n");
+                print "Error: No such CGI app - $req_params{SCRIPT_FILENAME} may not exist or is not executable by this process.\n";
+            }
+
+        }
+}
+
